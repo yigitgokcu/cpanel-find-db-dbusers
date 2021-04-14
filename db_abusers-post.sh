10d66612f538128966e4498e08d2a981df8a2410
Yiğit Gökçü <59539521+yigitgokcu@users.noreply.github.com>
2021-04-14 15:25:27 +0300

    Update db_abusers-post.sh

--- db_abusers-post.sh
+++ db_abusers-post.sh
@@ -8,7 +8,7 @@ SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 
 WEBHOOK_URL=
 
-sudo sh /path/to/zabbix/scripts/db_abusers/zabbix_db-abusers.sh | sort -nr | head -n 10 > /path/to/zabbix/scripts/db_abusers/$(date +%Y%m%d-%H-%M)-db_abusers.txt
-sudo curl -X POST -H 'Content-type: application/json' --data "{\"text\": \" \n :warning: $(printf "**Top DB Users by MySQL Queries on `hostname`** \n\n" && cat $(find /path/to/zabbix/scripts/db_abusers/ -name "*.txt" -type f -exec ls -at {} + | head -1))\"}" https://$WEBHOOK_URL
+sudo sh /path/to/db_abusers/db-abusers.sh | sort -nr | head -n 10 > /path/to/db_abusers/$(date +%Y%m%d-%H-%M)-db_abusers.txt
+sudo curl -X POST -H 'Content-type: application/json' --data "{\"text\": \" \n :warning: $(printf "**Top DB Users by MySQL Queries on `hostname`** \n\n" && cat $(find /path/to/db_abusers/ -name "*.txt" -type f -exec ls -at {} + | head -1))\"}" https://$WEBHOOK_URL
 
 exit
